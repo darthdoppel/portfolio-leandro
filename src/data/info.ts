c54693eff761af1379808e63dd7a5546627ce9af
Leandro Magallanes <leandromartinm7@gmail.com>
2023-09-25 13:32:54 -0300

    changes

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,97 +1,72 @@
 export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
+  name: "Leandro Magallanes",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  about: `
+  and currently exploring opportunities as a Fullstack Developer, I have a strong inclination towards Backend development, while my skills in Frontend, enriched by libraries like NextUI and the use of TailwindCSS, ensure adaptability to a variety of project requirements. My tech journey involves self-learning and implementing technologies like JavaScript, React, TypeScript, NodeJS, MongoDB, and ExpressJS. My learning quest continues, leading to the successful completion of several independent web projects. I am passionate about devising solutions that address real-world problems, striving to make people’s work less repetitive and more focused on creating value.
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
+      name: "Coderhouse",
+      location: "Remote",
+      startDate: "Jan 2023",
       endDate: "Present",
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+        "- Tutoring in Tableau within the online platform of courses and careers at Coderhouse.",
+        "- Handling students and supporting professors, providing live practice space with students, correcting assignments and pre-deliveries.",
       ],
     },
-
     {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
+      name: "Freelance",
+      location: "Argentina",
+      startDate: "Jun 2015",
+      endDate: "Present",
       description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
+        "- Repairing computer equipment, administrating Windows and Linux.",
+        "- Resolving hardware and software issues.",
       ],
     },
-
     {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
+      name: "Tu Service S.R.L.",
+      location: "Argentina",
+      startDate: "Apr 2015",
+      endDate: "Mar 2023",
       description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+        "- In charge of the administration sector and general invoicing of the company.",
+        "- Drafting technical reports for presentation to Insurance Companies and analysis of claims.",
+        "- Quoting goods, replacement to new, used, with depreciation for use and age.",
+        "- Contact with laboratories (workshops) for inspection of goods for coverage analysis.",
       ],
-    },
+    }
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
+      name: "Escuela Técnica Ing. E. M. Hermitte Nº 34",
+      location: "Argentina",
+      startDate: "2008",
+      endDate: "2014",
       description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+        "Major: Master Builder.",
+        "Field: Construction/Construction Technician.",
       ],
-    },
-    {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
     },
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    github: "https://github.com/darthdoppel",
+    email: "leandromartinm7@gmail.com",
+    linkedin: "https://www.linkedin.com/in/leandro-magallanes-681902173/",
   },
 
   projects: [
     {
-      title: "Astro Portfolio",
+      title: "Stock App",
       isFeatured: true,
-      thumbnail: "/assets/images/astro-portfolio.png",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
-      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+      thumbnail: "/assets/images/stockappthumb.png",
+      githubUrl: "https://github.com/darthdoppel/stock-app",
+      liveUrl: "https://stock-app-client.onrender.com/",
     },
   ],
 };
